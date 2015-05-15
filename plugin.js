d92ffc16f49558561b0a2df947966b960605d436
kpawel-29 <pakadlubowski@gmail.com>
2015-05-15 10:10:46 +0200

    add email validation

--- plugin.js
+++ plugin.js
@@ -1,3 +1,21 @@
-$(document).ready(function () {
-	"use strict"
-});
+(function($) {
+
+	$.fn.validate = function () {
+		/*
+		 * http://stackoverflow.com/questions/2507030/email-validation-using-jquery
+		 */
+		function IsEmail(email) {
+		  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+		  return regex.test(email);
+		}
+
+		$(this).keyup(function() {
+			var email = $(this).val();
+			if(!IsEmail(email)){
+				$(this).css("border", "2px solid red");
+			} else {
+				$(this).css("border", "");
+			}
+		})
+	}
+}) (jQuery);
