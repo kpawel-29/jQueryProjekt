7d97da462ab4ac70921c103891ad506373b24e22
kpawel-29 <pakadlubowski@gmail.com>
2015-05-17 22:11:06 +0200

    add editSubmitBtn function

--- plugin.js
+++ plugin.js
@@ -7,14 +7,21 @@
 		function IsEmail(email) {
 		  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 		  return regex.test(email);
-		}
+		};
+
+		function editSubmitBtn(value) {
+			var btn = $("button");
+			btn.attr("disabled", value);
+		};
 
 		$(this).keyup(function() {
 			var email = $(this).val();
 			if(!IsEmail(email)){
 				$(this).css("border", "2px solid red");
+				editSubmitBtn(true);
 			} else {
 				$(this).css("border", "");
+				editSubmitBtn(false);
 			}
 		})
 	}
